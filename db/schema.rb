e722f75843e3f907c6df4332d982e5dd179e6a70
masamune17 <2396niisankyumu@gmail.com>
2022-03-22 22:25:32 +0900

    タスクの名前にユニーク制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_22_130213) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_22_132137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_130213) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name", unique: true
   end
 
 end
